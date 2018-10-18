048b7d6a6f8884ec759f15599603012784c087b0
Ali Babaei <alibabaei12@gmail.com>
2018-10-18 14:01:37 -0700

    register page compeleted

--- SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/RegisterActivity.java
+++ SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/RegisterActivity.java
@@ -1,10 +1,22 @@
 package com.example.slugbooks.slugbooks;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -18,7 +30,8 @@ public class RegisterActivity extends AppCompatActivity {
     private String emailStr;
     private String passwordStr;
     private String repeatPasswordStr;
-
+    private FirebaseAuth mAuth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +43,10 @@ public class RegisterActivity extends AppCompatActivity {
         repeatPasswordEditText = (EditText)findViewById(R.id.repeatPasswordEditTextId);
 
         registerButton = (Button) findViewById(R.id.registerButtonID);
+
+        //initialize the firebase auth
+        mAuth = FirebaseAuth.getInstance();
+
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,8 +60,57 @@ public class RegisterActivity extends AppCompatActivity {
                 //test to see if we got the right email
                 System.out.println("email: " + emailStr + "=========== password: " + passwordStr + " =============== repeat Password: " + repeatPasswordStr);
 
-
+                registerUser(emailStr, passwordStr, repeatPasswordStr);
             }
         });
+    }
+
+
+
+    private void registerUser(String emailStr, String passwordStr, String repeatPasswordStr) {
+
+        if (TextUtils.isEmpty(emailStr)) {
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            // stopping the function execution further
+        }
+        else if (TextUtils.isEmpty(passwordStr)) {
+            //password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            // stopping the function execution further
+        }
+        else if (TextUtils.isEmpty(repeatPasswordStr)) {
+            Toast.makeText(this, "Please repeat the password", Toast.LENGTH_SHORT).show();
+        }
+
+        //progressDialog.setMessage("Register User...");
+        //progressDialog.show();
+
+        else if (repeatPasswordStr.equals(passwordStr)) {
+
+
+            mAuth.createUserWithEmailAndPassword(emailStr, passwordStr)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //user is successfully registered and logged in
+                                // we will start profile activity here
+                                // right now lets display a toast only
+                                Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Could not Register...  Please try again", Toast.LENGTH_SHORT).show();
+
+                            }
+                            // ...
+                        }
+                    });
+
+        }
+        else{
+            Toast.makeText(this, "The Passwords Were Not A Mach! Try Again!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
