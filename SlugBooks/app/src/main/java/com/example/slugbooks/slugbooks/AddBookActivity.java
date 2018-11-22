b326eb961b3a4927b3c6f6c97903d38560c1a82c
Ali Babaei <alibabaei12@gmail.com>
2018-11-22 00:13:14 -0800

    the add book page UI complete

--- SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/AddBookActivity.java
+++ SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/AddBookActivity.java
@@ -10,37 +10,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddBookActivity extends AppCompatActivity {
 
     private Uri chosenImage;
-    Button addImage;
-    EditText bookName;
-    EditText author;
-    EditText descrip;
+    Button addImageButton;
+    EditText bookNameEditText;
+    EditText authorEditText;
+    EditText descripEditText;
     EditText classEditText;
-    EditText edition;
-    EditText condition;
-    EditText price;
+    EditText editionEditText;
+    EditText conditionEditText;
+    EditText priceEditText;
 
+    LinearLayout linearLayout;
+    LinearLayout.LayoutParams layoutParams;
+
+    BookObject bookObject;
+    DataModel dataModel;
 
     private static final int CHOOSE_IMAGE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_book);
+        setContentView(R.layout.content_add_book);
 
+        addImageButton = (Button) findViewById(R.id.addImageButtonID);
+        bookNameEditText = (EditText) findViewById(R.id.bookNameEditID);
+        authorEditText = (EditText) findViewById(R.id.authorEditID);
+        descripEditText = (EditText) findViewById(R.id.descriptionEditID);
+        classEditText = (EditText) findViewById(R.id.classEditID);
+        editionEditText = (EditText) findViewById(R.id.edititionEditID);
+        conditionEditText = (EditText) findViewById(R.id.conditionEditID);
+        priceEditText = (EditText) findViewById(R.id.priceEditID);
 
+        linearLayout = (LinearLayout) findViewById(R.id.addImageLayoutID);
 
+        layoutParams = new LinearLayout.LayoutParams(200, 200);
+        layoutParams.setMargins(20, 0, 40, 0);
     }
 
     //get the image from gllery
-    private void openPhotos(View view) {
 
+    public void openP(View view) {
         openGallery();
-
     }
-
     private void openGallery()
     {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
@@ -53,17 +72,31 @@ public class AddBookActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //Detects request codes
+
         if(resultCode==RESULT_OK && requestCode == CHOOSE_IMAGE) {
-          /*  selectedImage = data.getData();
+            Uri selectedImage = data.getData();
             String ali = selectedImage.getPath();
 
-            bookImage.setImageURI(selectedImage);
+            ImageView newBookImg = new ImageView(this);
 
-            System.out.println("URL is : " + imageURLstr);
+            newBookImg.setImageURI(selectedImage);
+            newBookImg.setLayoutParams(layoutParams);
 
+            linearLayout.addView(newBookImg);
 
-            // bookImage.setImageURI(selectedImage);*/
         }
     }
+
+    public void post(View view) {
+        List<String> imgUrls = new ArrayList<String>();
+        bookObject = new BookObject(bookNameEditText.getText().toString(),authorEditText.getText().toString(),
+                                descripEditText.getText().toString(),classEditText.getText().toString(),editionEditText.getText().toString(),
+                                conditionEditText.getText().toString(),priceEditText.getText().toString(),imgUrls);
+
+
+
+    }
+
+
 }
 
