a24dbfd1363b2479700da6566290492d41fa68dc
Christina <chensonc@ucsc.edu>
2018-11-22 14:07:04 -0800

    finished implementing bottom navigation

--- SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/InboxActivity.java
+++ SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/InboxActivity.java
@@ -1,9 +1,14 @@
 package com.example.slugbooks.slugbooks;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import java.util.HashMap;
@@ -27,5 +32,39 @@ public class InboxActivity extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
         mTabLayout = findViewById(R.id.main_tabs);
         mTabLayout.setupWithViewPager(mViewPager);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        Intent intent1 = new Intent(InboxActivity.this, HomeActivity.class);
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.action_sell:
+                        Intent intent2 = new Intent(InboxActivity.this, AddBookActivity.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.action_messages:
+                        break;
+
+                    case R.id.action_profile:
+                        Intent intent4 = new Intent(InboxActivity.this, ProfileActivity.class);
+                        startActivity(intent4);
+                        break;
+                }
+
+
+                return false;
+            }
+        });
     }
 }
