5f1fb5aeacea69cf8cd217c9deca113e052147aa
Ali Babaei <alibabaei12@gmail.com>
2018-11-28 13:09:59 -0800

    after delete goes to main page

--- SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/editBookActivity.java
+++ SlugBooks/app/src/main/java/com/example/slugbooks/slugbooks/editBookActivity.java
@@ -1,5 +1,6 @@
 package com.example.slugbooks.slugbooks;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -77,7 +78,7 @@ public class editBookActivity extends AppCompatActivity  {
                          dataModel.setBookObject(bookObjects);
                          databaseReference.child("users").child(firebaseAuth.getUid()).setValue(dataModel);
 
-
+                            startActivity(new Intent(editBookActivity.this,HomeActivity.class));
 
                     }
                     else{
